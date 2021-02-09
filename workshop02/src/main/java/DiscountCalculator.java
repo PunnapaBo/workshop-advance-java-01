cee1c87ce3d2ebea69d4a4b8f294a8fdeaa1981e
punnapa.bo <punnapa.bo@kbtg.tech>
2021-02-09 10:39:02 +0700

    second commit

---
+++ workshop02/src/main/java/DiscountCalculator.java
@@ -0,0 +1,6 @@
+public class DiscountCalculator {
+    public static int get(Basket basket) {
+        // Logic
+        return 0;
+    }
+}
---
+++ workshop02/src/main/java/PriceCalculator.java
@@ -0,0 +1,7 @@
+public class PriceCalculator {
+
+    public static int get(Basket basket) {
+        // Logic
+        return 0;
+    }
+}
