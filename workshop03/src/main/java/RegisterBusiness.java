9ba194d7dc0a8ff5dc25df21500028812a30f28c
punnapa.bo <punnapa.bo@kbtg.tech>
2021-02-09 17:21:11 +0700

    refactor code

--- workshop03/src/main/java/RegisterBusiness.java
+++ workshop03/src/main/java/RegisterBusiness.java
@@ -3,12 +3,20 @@ import java.util.Arrays;
 public class RegisterBusiness {
 
     public Integer register(SpeakerRepository repository, Speaker speaker) {
-        Integer speakerId;
-        String[] domains = {"gmail.com", "live.com"};
+        Integer speakerId = null;
+        validateRegisterData(speaker);
 
-        if (speaker.getFirstName() != null && !speaker.getFirstName().trim().equals("")) {
-            if (speaker.getLastName() != null && !speaker.getLastName().trim().equals("")) {
-                if (speaker.getEmail() != null && !speaker.getEmail().trim().equals("")) {
+        int exp = speaker.getExp();
+        speaker.setRegistrationFee(calculateFee( exp));
+        try {
+            speakerId = repository.saveSpeaker(speaker);
+        }catch (Exception exception) {
+            throw new SaveSpeakerException("Can't save a speaker.");
+        }
+
+       /* if (isFirstNameNotBlank(speaker.getFirstName())) {
+            if (isLastNameNotBlank(speaker.getLastName())) {
+                if (isEmailNotBlank(speaker.getEmail())) {
                     String emailDomain = getEmailDomain(speaker.getEmail()); // outofbound
                     if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() == 1) {
                         int exp = speaker.getExp();
@@ -30,8 +38,42 @@ public class RegisterBusiness {
         } else {
             throw new ArgumentNullException("First name is required.");
         }
-
+*/
         return speakerId;
+    }
+
+    private void validateRegisterData(Speaker speaker) {
+        if(isFirstNameBlank(speaker.getFirstName())){
+            throw new ArgumentNullException("First name is required.");
+        }
+        if(isLastNameBlank(speaker.getLastName())){
+            throw new ArgumentNullException("Last name is required.");
+        }
+        if(isEmailBlank(speaker.getEmail())){
+            throw new ArgumentNullException("Email is required.");
+        }
+        String[] domains = {"gmail.com", "live.com"};
+        String emailDomain = getEmailDomain(speaker.getEmail());
+        if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() == 0) {
+            throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
+        }
+    }
+    private boolean isNullOrEmpty(String input) {
+        return input == null || input.trim().equals("");
+    }
+
+
+    private boolean isEmailBlank(String email) {
+        return isNullOrEmpty(email);
+    }
+
+
+    private boolean isLastNameBlank(String lastName) {
+        return isNullOrEmpty(lastName);
+    }
+
+    private boolean isFirstNameBlank(String firstName) {
+        return isNullOrEmpty(firstName);
     }
 
     public int calculateFee( int exp) {
